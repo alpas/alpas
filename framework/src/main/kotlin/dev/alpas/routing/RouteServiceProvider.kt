0b7612ffda27094abac9c4304950cc497fdca990
Ashok Gelal <ashokgelal@gmail.com>
2019-11-18 20:21:14 -0700

    Add loadRoutes method that an app could override to load routes

--- framework/src/main/kotlin/dev/alpas/routing/RouteServiceProvider.kt
+++ framework/src/main/kotlin/dev/alpas/routing/RouteServiceProvider.kt
@@ -1,21 +1,23 @@
 package dev.alpas.routing
 
-import dev.alpas.Application
-import dev.alpas.PackageClassLoader
-import dev.alpas.ServiceProvider
+import dev.alpas.*
 import dev.alpas.auth.AuthConfig
 import dev.alpas.auth.console.MakeAuthCommand
-import dev.alpas.config
 import dev.alpas.console.Command
-import dev.alpas.make
 import dev.alpas.routing.console.MakeControllerCommand
 import dev.alpas.routing.console.MakeMiddlewareCommand
 import dev.alpas.routing.console.MakeValidationGuardCommand
 import dev.alpas.routing.console.RouteListCommand
 
+@Suppress("unused")
 open class RouteServiceProvider : ServiceProvider {
     override fun register(app: Application) {
-        app.singleton(Router())
+        val router = Router()
+        app.singleton(router)
+        loadRoutes(router)
+    }
+
+    protected open fun loadRoutes(router: Router) {
     }
 
     override fun commands(app: Application): List<Command> {
