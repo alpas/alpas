a8c48f0b5e254631b61d4508c7c0fae9047cd53e
Ashok Gelal <ashokgelal@gmail.com>
2019-12-01 15:03:40 -0700

    Make column name optional. If it is null, assume column name to be same as the attribute.

--- framework/src/main/kotlin/dev/alpas/ozone/validation/Unique.kt
+++ framework/src/main/kotlin/dev/alpas/ozone/validation/Unique.kt
@@ -3,27 +3,29 @@ package dev.alpas.ozone.validation
 import dev.alpas.http.HttpCall
 import dev.alpas.validation.ErrorMessage
 import dev.alpas.validation.Rule
+import dev.alpas.validation.ValidationGuard
 import me.liuwj.ktorm.database.useConnection
 
 class Unique(
     private val table: String,
-    private val column: String,
+    private val column: String? = null,
     private val ignore: String? = null,
     private val message: ErrorMessage = null
 ) : Rule() {
     override fun check(attribute: String, call: HttpCall): Boolean {
         val value = call.param(attribute)
         val exists = useConnection {
-            var query = "SELECT COUNT(1) FROM `$table` WHERE `$column` = ?"
+            val columnToCheck = column ?: attribute
+            var query = "SELECT COUNT(1) FROM `$table` WHERE `$columnToCheck` = ?"
             val ignoreProps = ignore?.split(":") ?: emptyList()
             val shouldIgnoreField = ignoreProps.count() == 2
             if (shouldIgnoreField) {
-                query = "$query AND `${ignoreProps[0]}` != ?"
+                query = "$query AND `${ignoreProps[0].trim()}` != ?"
             }
             val statement = it.prepareStatement(query)
             statement.setObject(1, value)
             if (shouldIgnoreField) {
-                statement.setObject(2, ignoreProps[1])
+                statement.setObject(2, ignoreProps[1].trim())
             }
             statement.executeQuery().let { result ->
                 result.next()
@@ -36,4 +38,13 @@ class Unique(
             }
         }
     }
+}
+
+fun ValidationGuard.unique(
+    table: String,
+    column: String? = null,
+    ignore: String? = null,
+    message: ErrorMessage = null
+): Rule {
+    return rule(Unique(table, column, ignore, message))
 }
