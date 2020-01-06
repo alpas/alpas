9fd84516c0154e2e2d44ebd0c74bd74d556517bd
Ashok Gelal <ashokgelal@gmail.com>
2020-01-05 21:25:33 -0700

    Don't make PasswordResetTokens internal as it is needed by migration

--- framework/src/main/kotlin/dev/alpas/auth/PasswordResetTokens.kt
+++ framework/src/main/kotlin/dev/alpas/auth/PasswordResetTokens.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.`java-time`.timestamp
 import java.time.Instant
 
-internal object PasswordResetTokens : Table("password_reset_tokens") {
+object PasswordResetTokens : Table("password_reset_tokens") {
     val email = varchar("email", 255).index()
     val token = varchar("token", 255)
     val createdAt = timestamp("created_at").nullable()
