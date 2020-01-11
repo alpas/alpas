c99e32031279b48aa9ca1a5928377d9ccf0797b5
Ashok Gelal <ashokgelal@gmail.com>
2020-01-11 15:02:15 -0700

    Update Runner

--- runner/src/main/kotlin/start.kt
+++ runner/src/main/kotlin/start.kt
@@ -7,15 +7,15 @@ import dev.alpas.routing.Controller
 import dev.alpas.routing.Router
 
 fun main(args: Array<String>) {
-    Alpas(args) {
-        make<Router>() {
-            get("/", PageController::class)
-        }
-    }.ignite()
+    Alpas(args).router{appRoutes()}.ignite()
 }
 
 class PageController : Controller() {
     fun index(call: HttpCall) {
         call.reply("hello")
     }
+}
+
+fun Router.appRoutes() {
+    get("/", PageController::class)
 }
