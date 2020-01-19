7b77e66d29de4a67480b4406242d21ba98e11287
Ashok Gelal <ashokgelal@gmail.com>
2020-01-19 11:34:38 -0700

    Add Pipeline tests

--- framework/src/main/kotlin/dev/alpas/Pipeline.kt
+++ framework/src/main/kotlin/dev/alpas/Pipeline.kt
@@ -1,5 +1,6 @@
 package dev.alpas
 
+// A handler for forwarding an instance of T
 typealias Handler<T> = (T) -> Unit
 
 open class Middleware<T> : (T, Handler<T>) -> Unit {
@@ -8,20 +9,34 @@ open class Middleware<T> : (T, Handler<T>) -> Unit {
 }
 
 class Pipeline<T : Any> {
-    lateinit var passable: T
-    lateinit var pipes: Iterable<Middleware<T>>
+    private lateinit var passable: T
+    private lateinit var pipes: Iterable<Middleware<T>>
+    private var isInvoked = false
+
     fun send(passable: T): Pipeline<T> {
+        checkIfInvoked()
         this.passable = passable
         return this
     }
 
-    fun through(pipe: Middleware<T>, vararg pipes: Middleware<T>): Pipeline<T> {
-        return through(pipes.toMutableList().apply { add(0, pipe) })
+    private fun checkIfInvoked() {
+        if (isInvoked) {
+            throw IllegalStateException("Pipeline is already invoked!")
+        }
     }
 
-    fun through(pipes: Iterable<Middleware<T>>): Pipeline<T> {
-        this.pipes = pipes
-        return this
+    fun through(pipe: Middleware<T>, vararg pipes: Middleware<T>): Pipeline<T> {
+        checkIfInvoked()
+        return through(listOf(pipe) + pipes)
+    }
+
+    fun through(pipes: Iterable<Middleware<T>>) = apply {
+        checkIfInvoked()
+        if (this::pipes.isInitialized) {
+            this.pipes = this.pipes + pipes
+        } else {
+            this.pipes = pipes
+        }
     }
 
     private fun prepareDestination(destination: Handler<T>): (T) -> Unit {
@@ -37,7 +52,9 @@ class Pipeline<T : Any> {
     }
 
     fun then(destination: Handler<T>) {
+        checkIfInvoked()
         val pipeline = pipes.reversed().fold(prepareDestination(destination), carry())
         pipeline(passable)
+        isInvoked = true
     }
 }
