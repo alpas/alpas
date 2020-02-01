659623508cf8c90d70c925565af035e0b8a415c7
Ashok Gelal <ashokgelal@gmail.com>
2020-02-01 14:44:24 -0700

    Organize tests

---
+++ framework/src/test/kotlin/CommandTestBase.kt
@@ -0,0 +1,18 @@
+package dev.alpas.tests
+
+import dev.alpas.auth.console.MakeAuthCommand
+import dev.alpas.console.Command
+import org.junit.jupiter.api.AfterEach
+
+abstract class CommandTestBase {
+    protected var lastCommand: MakeAuthCommand? = null
+
+    @AfterEach
+    fun afterEach() {
+        lastCommand?.deleteResources()
+    }
+
+    private fun Command.deleteResources() {
+        resourcesDir.deleteRecursively()
+    }
+}
