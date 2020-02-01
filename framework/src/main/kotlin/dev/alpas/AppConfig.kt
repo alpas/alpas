3e59b89905feb99aac147dea5cfae3125df1191c
Ashok Gelal <ashokgelal@gmail.com>
2020-02-01 14:52:55 -0700

    Add some convenient and conventional methods to environment

--- framework/src/main/kotlin/dev/alpas/AppConfig.kt
+++ framework/src/main/kotlin/dev/alpas/AppConfig.kt
@@ -1,23 +1,23 @@
 package dev.alpas
 
-import java.nio.file.Paths
 import java.time.Duration
 import java.time.ZoneOffset
 
-open class AppConfig(env: Environment) : Config {
+open class AppConfig(private val env: Environment) : Config {
     open val enableNetworkShare = env("ENABLE_NETWORK_SHARE", false)
     open val appPort = env("APP_PORT", 8080)
     open val appUrl = env("APP_URL", "")
     open val connectionTimeOut: Duration = Duration.ofMinutes(1)
-    open val staticDirs by lazy {
-        val dirs = arrayOf("/web", Paths.get(env.storagePath, "app", "public").toAbsolutePath().toString()) +
-                extraStaticDirs()
-        if (enableStorageWebDirectory) {
-            arrayOf(storageWebDirectory) + dirs
-        } else {
-            dirs
+    open val staticDirs: Array<String>
+        get() {
+            val dirs = arrayOf("/web", env.storagePath("app", "public")) +
+                    extraStaticDirs()
+            return if (enableStorageWebDirectory) {
+                arrayOf(storageWebDirectory) + dirs
+            } else {
+                dirs
+            }
         }
-    }
     open val encryptionKey = env("APP_KEY")
     open val maxThreads = env("APP_MAX_THREADS", 200)
     open val minThreads = env("APP_MIN_THREADS", 8)
@@ -28,11 +28,12 @@ open class AppConfig(env: Environment) : Config {
     open val allowMethodSpoofing = true
     open val throwOnMissingStaticDirectories = false
     open val enableStorageWebDirectory = env.isDev
-    open val storageWebDirectory by lazy {
-        // For this you need to create a symlink from storage/src/main/resources/web to src/main/resources/web
-        // mkdir -p $PWD/storage/src/main/resources && ln -s $PWD/src/main/resources/web $PWD/storage/src/main/resources/web
-        Paths.get(env.storagePath, "src", "main", "resources", "web").toAbsolutePath().toString()
-    }
+    open val storageWebDirectory: String
+        get() {
+            // For this you need to create a symlink from storage/src/main/resources/web to src/main/resources/web
+            // mkdir -p $PWD/storage/src/main/resources && ln -s $PWD/src/main/resources/web $PWD/storage/src/main/resources/web
+            return env.storagePath(*RESOURCES_DIRS, "web")
+        }
 
     protected open fun extraStaticDirs() = emptyArray<String>()
 }
