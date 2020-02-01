448d2c5217613d8331cefbcebf4b945d964914fe
Ashok Gelal <ashokgelal@gmail.com>
2020-01-31 21:41:24 -0700

    Add make:factory command

---
+++ framework/src/main/kotlin/dev/alpas/ozone/console/stubs/FactoryStubs.kt
@@ -0,0 +1,30 @@
+package dev.alpas.ozone.console.stubs
+
+internal class FactoryStubs {
+    companion object {
+        fun stub(): String {
+            return """
+                package StubPackageName
+
+                import dev.alpas.ozone.EntityFactory
+                import dev.alpas.ozone.faker
+                import StubEntitiesPackage.StubEntityClazzName
+                import StubEntitiesPackage.StubTableClazzName
+
+                internal class StubClazzName : EntityFactory<StubEntityClazzName> {
+                    override val table = StubTableClazzName
+                    
+                    override fun entity(): StubEntityClazzName {
+                        // https://alpas.dev/docs/ozone
+
+                        return StubEntityClazzName {
+                            // name = faker.name().name()
+                            // updatedAt = faker.date().past(1, TimeUnit.HOURS).toInstant()
+                            // createdAt = Instant.now()
+                        }
+                    }
+                }
+            """.trimIndent()
+        }
+    }
+}
