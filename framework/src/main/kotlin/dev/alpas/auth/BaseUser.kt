ab01bc395e4205ca7326f4b7d048d060cd3749ef
Ashok Gelal <ashokgelal@gmail.com>
2020-02-08 12:38:56 -0700

    Include date fields

--- framework/src/main/kotlin/dev/alpas/auth/BaseUser.kt
+++ framework/src/main/kotlin/dev/alpas/auth/BaseUser.kt
@@ -1,10 +1,13 @@
 package dev.alpas.auth
 
 import dev.alpas.ozone.Ozone
+import java.time.Instant
 
 interface BaseUser<E : BaseUser<E>> : Ozone<E>, Authenticatable {
     override var id: Long
     override var password: String
     override var email: String?
     var name: String?
+    var createdAt: Instant?
+    var updatedAt: Instant?
 }
