ba8fb2ad2c1445a73b70f138b716f99ddd463c23
Ashok Gelal <ashokgelal@gmail.com>
2020-02-09 16:19:19 -0700

    Move users related table to base to allow to be easily extendable

--- framework/src/main/kotlin/dev/alpas/auth/BaseUser.kt
+++ framework/src/main/kotlin/dev/alpas/auth/BaseUser.kt
@@ -1,5 +1,6 @@
 package dev.alpas.auth
 
+import dev.alpas.md5
 import dev.alpas.ozone.Ozone
 import java.time.Instant
 
@@ -10,4 +11,13 @@ interface BaseUser<E : BaseUser<E>> : Ozone<E>, Authenticatable {
     var name: String?
     var createdAt: Instant?
     var updatedAt: Instant?
+    val emailVerifiedAt: Instant?
+
+    @ExperimentalUnsignedTypes
+    fun gravatarUrl(): String {
+        val hash = email?.trim()?.toLowerCase()?.md5() ?: ""
+        return "//www.gravatar.com/avatar/$hash?s=160&d=robohash"
+    }
+
+    override fun isEmailVerified() = emailVerifiedAt != null
 }
--- framework/src/main/kotlin/dev/alpas/auth/UsersTable.kt
+++ framework/src/main/kotlin/dev/alpas/auth/UsersTable.kt
@@ -3,18 +3,23 @@ package dev.alpas.auth
 import dev.alpas.ozone.Ozone
 import dev.alpas.ozone.OzoneTable
 import dev.alpas.ozone.bigIncrements
+import me.liuwj.ktorm.dsl.eq
+import me.liuwj.ktorm.entity.findById
+import me.liuwj.ktorm.entity.findOne
 import me.liuwj.ktorm.schema.Column
 import me.liuwj.ktorm.schema.SqlType
+import me.liuwj.ktorm.schema.timestamp
 import me.liuwj.ktorm.schema.varchar
 import java.util.*
 
-open class BaseUsersTable<E : BaseUser<E>> : OzoneTable<E>("users") {
-    open val id by bigIncrements().bindTo { it.id }
+open class BaseUsersTable<E : BaseUser<E>> : OzoneTable<E>("users"), UserProvider {
+    open val id by bigIncrements()
     open val password by varchar("password").bindTo { it.password }
     open val email by varchar("email").bindTo { it.email }
     open val name by varchar("name").bindTo { it.name }
     open val createdAt by createdAt()
     open val updatedAt by updatedAt()
+    open val emailVerifiedAt by timestamp("email_verified_at").nullable().bindTo { it.emailVerifiedAt }
 
     fun <T : Any> bind(name: String, ofType: SqlType<T>): Column<*> {
         return try {
@@ -22,6 +27,14 @@ open class BaseUsersTable<E : BaseUser<E>> : OzoneTable<E>("users") {
         } catch (e: NoSuchElementException) {
             this.registerColumn(name, ofType).getColumn()
         }
+    }
+
+    override fun findByUsername(username: Any): E? {
+        return findOne { it.email.eq(username.toString()) }
+    }
+
+    override fun findByPrimaryKey(id: Any): E? {
+        return findById(id)
     }
 }
 
