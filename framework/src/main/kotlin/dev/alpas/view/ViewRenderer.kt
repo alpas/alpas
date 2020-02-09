dc1abd261b1b6d35d6e331fb9be3409b9830f16c
Vanessa Gertman <vanessagertman@gmail.com>
2020-02-09 16:23:45 -0700

    Give each viewRenderer function its own name to fix overriding errors

--- framework/src/main/kotlin/dev/alpas/view/ViewRenderer.kt
+++ framework/src/main/kotlin/dev/alpas/view/ViewRenderer.kt
@@ -6,15 +6,15 @@ import dev.alpas.validation.SharedDataBag
 
 interface ViewRenderer {
     fun extend(extension: AbstractExtension, vararg extensions: AbstractExtension)
-    fun render(viewName: String?, sharedDataBag: SharedDataBag, viewArgs: Map<String, Any?>? = null): String {
+    fun renderView(viewName: String?, sharedDataBag: SharedDataBag, viewArgs: Map<String, Any?>? = null): String {
         TODO()
     }
 
-    fun render(template: String, args: Map<String, Any?>? = null): String {
+    fun renderTemplate(template: String, sharedDataBag: SharedDataBag, args: Map<String, Any?>? = null): String {
         TODO()
     }
 
-    fun render(context: RenderContext, viewArgs: Map<String, Any?>? = null): String {
-        return render(context.viewName, context.sharedDataBag, viewArgs)
+    fun renderContext(context: RenderContext, viewArgs: Map<String, Any?>? = null): String {
+        return renderView(context.viewName, context.sharedDataBag, viewArgs)
     }
 }
