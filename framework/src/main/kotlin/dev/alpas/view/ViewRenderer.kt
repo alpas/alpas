2d3402279acf391714e261335dfc767486513933
Ashok Gelal <ashokgelal@gmail.com>
2020-02-09 18:27:30 -0700

    Leave the old methods for now but mark them as deprecated.

--- framework/src/main/kotlin/dev/alpas/view/ViewRenderer.kt
+++ framework/src/main/kotlin/dev/alpas/view/ViewRenderer.kt
@@ -6,6 +6,23 @@ import dev.alpas.validation.SharedDataBag
 
 interface ViewRenderer {
     fun extend(extension: AbstractExtension, vararg extensions: AbstractExtension)
+
+    @Deprecated(
+        "This method is deprecated. Use renderView() instead.",
+        ReplaceWith("renderView(viewName, sharedDataBag, viewArgs)")
+    )
+    fun render(viewName: String?, sharedDataBag: SharedDataBag, viewArgs: Map<String, Any?>? = null): String {
+        return renderView(viewName, sharedDataBag, viewArgs)
+    }
+
+    @Deprecated(
+        "This method is deprecated. Use renderTemplate() instead.",
+        ReplaceWith("renderTemplate(template, SharedDataBag(), args)", "dev.alpas.validation.SharedDataBag")
+    )
+    fun render(template: String, args: Map<String, Any?>? = null): String {
+        return renderTemplate(template, SharedDataBag(), args)
+    }
+
     fun renderView(viewName: String?, sharedDataBag: SharedDataBag, viewArgs: Map<String, Any?>? = null): String {
         TODO()
     }
