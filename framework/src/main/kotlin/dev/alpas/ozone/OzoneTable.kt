c67140fff969a7381dcbf177176837a1cebd7292
Ashok Gelal <ashokgelal@gmail.com>
2020-02-16 20:26:37 -0700

    Cleanup table. Add comments

--- framework/src/main/kotlin/dev/alpas/ozone/OzoneTable.kt
+++ framework/src/main/kotlin/dev/alpas/ozone/OzoneTable.kt
@@ -5,7 +5,7 @@ import me.liuwj.ktorm.dsl.AssignmentsBuilder
 import me.liuwj.ktorm.dsl.combineConditions
 import me.liuwj.ktorm.dsl.eq
 import me.liuwj.ktorm.dsl.isNull
-import me.liuwj.ktorm.entity.Entity
+import me.liuwj.ktorm.entity.findList
 import me.liuwj.ktorm.entity.findOne
 import me.liuwj.ktorm.expression.ArgumentExpression
 import me.liuwj.ktorm.expression.ColumnAssignmentExpression
@@ -15,22 +15,45 @@ import java.time.temporal.Temporal
 import kotlin.reflect.KClass
 import kotlin.reflect.full.memberProperties
 
+/**
+ * Ozone's base table that enhances Ktorm's table by adding few super helpful extensions. It is recommended to
+ * extend from this table rather than extending Ktorm's table as most of the extensions are available only on
+ * this table. This table brings some other features as well such as migrations, relationship extensions, etc.
+ *
+ * @param tableName The name of the table.
+ * @param alias The alias of this table.
+ * @param entityClass The entity class for this table.
+ */
 abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null, entityClass: KClass<E>? = null) :
     Table<E>(tableName, alias, entityClass) {
 
-    protected open val autoBindColumnNames = arrayOf("id", "created_at", "updated_at")
-
+    /**
+     * Column metadata for this table. The metadata is used during migration.
+     */
     internal val metadataMap = hashMapOf<String, ColumnMetadata>()
 
+    /**
+     * The name of the columns that will be bound automatically to their corresponding entity
+     * properties. Override this method to decide what columns would be bound automatically.
+     */
+    protected open val autoBindColumnNames = arrayOf("id", "created_at", "updated_at")
+
+    /**
+     * Whether to autobind the given column name or not. Instead of overriding [autoBindColumnNames],
+     * you can instead override this method and decide per column whether to autobind or not. Currently,
+     * only `id`, `created_at`, and `updated_at`
+     */
     open fun shouldAutoBind(column: String): Boolean {
         return autoBindColumnNames.contains(column)
     }
 
-    inline fun <C : Any, R : Ozone<R>> ColumnRegistration<C>.belongsTo(
-        referenceTable: Table<R>,
-        selector: (E) -> R?
-    ) = apply { references(referenceTable, selector) }
-
+    // COLUMN REGISTRATION EXTENSIONS
+    /**
+     * Set the column's size metadata to the given size. This metadata can be applied only to
+     * the columns of type string such as  [MediumTextSqlType], [LongTextSqlType], etc.
+     *
+     * @param size Size of the column.
+     */
     fun ColumnRegistration<String>.size(size: Int): ColumnRegistration<String> {
         val columnName = getColumn().name
         val metadata = metadataMap[columnName]
@@ -43,6 +66,12 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
         return this
     }
 
+    /**
+     * Set the column's default value metadata to the given value.
+     * This metadata can be applied to any [SqlType] columns.
+     *
+     * @param default The default value for the column.
+     */
     fun <T : Any> ColumnRegistration<T>.default(default: T): ColumnRegistration<T> {
         val columnName = getColumn().name
         val metadata = metadataMap[columnName]
@@ -55,6 +84,11 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
         return this
     }
 
+    /**
+     * Set the column's default value to use current. This metadata can be applied to only
+     * the column of [Temporal] sql type such as created_at, updated_at, etc.
+     *
+     */
     fun <T : Temporal> ColumnRegistration<T>.useCurrent(): ColumnRegistration<T> {
         val columnName = getColumn().name
         val metadata = metadataMap[columnName]
@@ -67,6 +101,11 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
         return this
     }
 
+    /**
+     * Make the column an unsigned column. This can be applied to any column
+     * of [Number] sql type such as Int, Long etc.
+     *
+     */
     fun <T : Number> ColumnRegistration<T>.unsigned(): ColumnRegistration<T> {
         val columnName = getColumn().name
         val metadata = metadataMap[columnName]
@@ -79,7 +118,14 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
         return this
     }
 
-    // todo: allow to pass `isUnique` param? as an autoincrement must be defined as a key
+    /**
+     *
+     * Make this column an autoincrement column. This can be applied to any column
+     * of [Number] sql type such as Int, Long etc.
+     *
+     * TODO: allow to pass `isUnique` param? as an autoincrement must be defined as a key
+     *
+     */
     fun <T : Number> ColumnRegistration<T>.autoIncrement(): ColumnRegistration<T> {
         val columnName = getColumn().name
         val metadata = metadataMap[columnName]
@@ -92,6 +138,9 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
         return this
     }
 
+    /**
+     * Mark this column as nullable. This can be applied to any column type.
+     */
     fun <T : Any> ColumnRegistration<T>.nullable(): ColumnRegistration<T> {
         val columnName = getColumn().name
         val metadata = metadataMap[columnName]
@@ -104,6 +153,12 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
         return this
     }
 
+    /**
+     * Set the precision of a double/float/decimal column type.
+     *
+     * @param total The total precision metadata for the column.
+     * @param places The number of decimal places for the column.
+     */
     fun <T : Number> ColumnRegistration<T>.precision(total: Int, places: Int): ColumnRegistration<T> {
         when (getColumn().sqlType) {
             !is FloatSqlType, !is DoubleSqlType, !is DecimalSqlType -> return this
@@ -119,6 +174,9 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
         return this
     }
 
+    /**
+     * Mark the column as unique. This can be applied to any column type.
+     */
     fun <T : Any> ColumnRegistration<T>.unique(): ColumnRegistration<T> {
         val columnName = getColumn().name
         val metadata = metadataMap[columnName]
@@ -131,6 +189,9 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
         return this
     }
 
+    /**
+     * Add an index to this column.
+     */
     fun <T : Any> ColumnRegistration<T>.index(): ColumnRegistration<T> {
         val columnName = getColumn().name
         val metadata = metadataMap[columnName]
@@ -204,11 +265,61 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
         }
     }
 
+    /**
+     * Bind this column to the reference table basically setting the belongsTo relationship.
+     */
+    inline fun <C : Any, R : Ozone<R>> ColumnRegistration<C>.belongsTo(
+        referenceTable: Table<R>,
+        selector: (E) -> R?
+    ) = apply { references(referenceTable, selector) }
+
+    /**
+     * Find an entity with the given [whereAttributes]. If the entity doesn't exist, create a
+     * new entity by combining both [whereAttributes] and [attributes]. Any extra
+     * attributes will be skipped inserting into the database.
+     *
+     * @param whereAttributes The attributes to use for looking up an entity.
+     * @param attributes The attributes to use while creating an entity.
+     *
+     * @return A existing or a new entity.
+     *
+     */
     @Suppress("UNCHECKED_CAST")
     fun findOrCreate(whereAttributes: Map<String, Any?>, attributes: Map<String, Any?> = emptyMap()): E {
         if (whereAttributes.isEmpty()) {
             throw IllegalArgumentException("whereAttributes cannot be empty.")
         }
+        return findOne(whereAttributes) ?: create(whereAttributes.plus(attributes))
+    }
+
+    /**
+     * Find an entity with the given [whereAttributes]. If the entity doesn't exist, create a
+     * new entity by combining both [whereAttributes] and the [assignmentBlock]. Any extra
+     * attributes will be skipped inserting into the database.
+     *
+     * @param whereAttributes The attributes to use for looking up an entity.
+     * @param assignmentBlock An assignment block for adding extra attributes while creating an entity.
+     *
+     * @return A existing or a new entity.
+     *
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun findOrCreate(
+        whereAttributes: Map<String, Any?>,
+        assignmentBlock: AssignmentsBuilder.(OzoneTable<E>) -> Unit
+    ): E {
+        return findOne(whereAttributes) ?: create(whereAttributes, assignmentBlock)
+    }
+
+    /**
+     * Find an entity with the given [whereAttributes].
+     *
+     * @param whereAttributes The attributes to use for looking up an entity.
+     *
+     * @return A existing entity if exists otherwise null.
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun findOne(whereAttributes: Map<String, Any?>): E? {
         val table = this
         val whereConditions = whereAttributes.map {
             val col = table[it.key] as Column<Any>
@@ -218,16 +329,33 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
             }
         }
         return findOne { whereConditions.combineConditions() }
-            ?: create { builder ->
-                val allAttributes = whereAttributes.plus(attributes)
-                for ((name, value) in allAttributes) {
-                    table.columns.find { it.name == name }?.let {
-                        builder[name] to value
-                    }
-                }
-            }
     }
 
+    /**
+     * Find many entities with the given [whereAttributes].
+     *
+     * @param whereAttributes The attributes to use for looking up an entity.
+     *
+     * @return A list of entities that satisfy the given [whereAttributes].
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun findMany(whereAttributes: Map<String, Any?>): List<E> {
+        val table = this
+        val whereConditions = whereAttributes.map {
+            val col = table[it.key] as Column<Any>
+            when (val value = it.value) {
+                null -> col.isNull()
+                else -> col.eq(value)
+            }
+        }
+        return findList { whereConditions.combineConditions() }
+    }
+
+    /**
+     * Create an entity with the given attributes. Any non-existent column names in the attributes map will be skipped.
+     *
+     * @return A new entity.
+     */
     @Suppress("UNCHECKED_CAST")
     fun create(attributes: Map<String, Any?>): E {
         val table = this
@@ -241,6 +369,22 @@ abstract class OzoneTable<E : Ozone<E>>(tableName: String, alias: String? = null
     }
 }
 
+/**
+ * Create an entity with the given attributes combined with the provided [AssignmentsBuilder]
+ * block. This is useful esp. when creating an entity with some HTTP field params and
+ * some additional assignments for the missing fields.
+ *
+ * ```kotlin
+ *
+ *  val input = call.params("name", "description", "website")
+ *  val app = Apps.create(input) {
+ *      it.ownerId to call.user.id
+ *  }
+ *
+ * ```
+ *
+ * @return A new entity.
+ */
 fun <E : Ozone<E>, T : OzoneTable<E>> T.create(
     attributes: Map<String, Any?> = emptyMap(),
     block: AssignmentsBuilder.(T) -> Unit
@@ -257,7 +401,7 @@ fun <E : Ozone<E>, T : OzoneTable<E>> T.create(
 /**
  * A map of an entity's column name to the actual corresponding column name in the table.
  */
-fun <T : Entity<T>> Table<T>.propertyNamesToColumnNames(): Map<String, String> {
+fun <T : Ozone<T>> OzoneTable<T>.propertyNamesToColumnNames(): Map<String, String> {
     return columns.map { col ->
         val colNameInTable = when (val binding = col.binding) {
             is NestedBinding -> {
@@ -268,18 +412,3 @@ fun <T : Entity<T>> Table<T>.propertyNamesToColumnNames(): Map<String, String> {
         Pair(colNameInTable, col.name)
     }.toMap()
 }
-
-data class ColumnMetadata(
-    val size: Int? = null,
-    val defaultValue: Any? = null,
-    val unsigned: Boolean = false,
-    val autoIncrement: Boolean = false,
-    val nullable: Boolean = false,
-    val unique: Boolean = false,
-    val index: Boolean = true,
-    val precision: Precision? = null,
-    val useCurrentTimestamp: Boolean = false,
-    internal val after: String? = null
-)
-
-data class Precision(val total: Int = 8, val places: Int = 2)
