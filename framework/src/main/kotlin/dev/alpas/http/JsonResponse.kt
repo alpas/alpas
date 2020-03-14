d9c3e5c5876330115ba641646123d661a5eb3d67
Ashok Gelal <ashokgelal@gmail.com>
2020-03-13 20:14:10 -0600

    Don't send the extra content to avoid sending unwanted data

--- framework/src/main/kotlin/dev/alpas/http/JsonResponse.kt
+++ framework/src/main/kotlin/dev/alpas/http/JsonResponse.kt
@@ -13,14 +13,11 @@ open class JsonResponse(
 
     override fun render(context: RenderContext, callback: InputStream.() -> Unit) {
         val output = when (payload) {
-            is JsonSerializable -> {
+            is JsonSerializable, is Map<*, *> -> {
                 payload.toJson()
             }
             is List<Any?> -> {
-                mapOf("payload" to payload, "extras" to context.sharedDataBag.all()).toJson()
-            }
-            is Map<*, *> -> {
-                payload.plus(context.sharedDataBag.all()).toJson()
+                mapOf("payload" to payload).toJson()
             }
             else -> {
                 payload?.toString()
