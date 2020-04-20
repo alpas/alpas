c3286991e48900bece529068f800fb2537b0d5e0
Ashok Gelal <ashokgelal@gmail.com>
2020-04-20 00:14:52 -0600

    cleanup serillzation

--- framework/src/main/kotlin/dev/alpas/JsonSerializer.kt
+++ framework/src/main/kotlin/dev/alpas/JsonSerializer.kt
@@ -1,34 +1,31 @@
 package dev.alpas
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import me.liuwj.ktorm.jackson.KtormModule
 
-class JsonSerializer {
+class JsonSerializer private constructor() {
     companion object {
-        val mapper = ObjectMapper().registerKotlinModule().registerKtormModule()
+        val mapper: ObjectMapper = ObjectMapper().registerKotlinModule().registerModule(KtormModule())
 
-        fun serialize(input: Any?, dti: ObjectMapper.DefaultTyping? = null): String {
-            return mapper.apply {
-                if (dti != null) {
-                    enableDefaultTyping(dti)
-                }
+        fun serialize(input: Any?, defaultTyping: ObjectMapper.DefaultTyping? = null): String {
+            if (input == null) return ""
+
+            return if (defaultTyping == null) {
+                mapper.copy().deactivateDefaultTyping()
+            } else {
+                mapper.copy().activateDefaultTyping(LaissezFaireSubTypeValidator(), defaultTyping)
             }.writeValueAsString(input)
         }
 
-        fun serialize(input: Map<*, *>): String {
-            return mapper.writeValueAsString(input)
-        }
-
-        inline fun <reified T> deserialize(input: String, dti: ObjectMapper.DefaultTyping? = null): T {
-            return mapper.apply {
-                if (dti != null) {
-                    enableDefaultTyping(dti)
-                }
+        inline fun <reified T> deserialize(input: String, defaultTyping: ObjectMapper.DefaultTyping? = null): T {
+            return if (defaultTyping == null) {
+                mapper.copy().deactivateDefaultTyping()
+            } else {
+                mapper.copy().activateDefaultTyping(LaissezFaireSubTypeValidator(), defaultTyping)
             }.readValue(input)
         }
     }
 }
-
-fun ObjectMapper.registerKtormModule(): ObjectMapper = this.registerModule(KtormModule())
